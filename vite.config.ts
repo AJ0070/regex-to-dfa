60048be3dd50bf7aa0e571a4a228e276685784f7
AJ0070 <jashambaliya1@gmail.com>
2026-03-24 09:00:37 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/regex-to-dfa/' : '/',
   server: {
-    allowedHosts: 'all',
+    allowedHosts: true,
   },
-});
+}));
